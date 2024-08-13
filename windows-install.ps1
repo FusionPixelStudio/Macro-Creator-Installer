922b3318d45935a74ab4476fd57086dfd0bb3b15
FusionPixelStudio <155030956+FusionPixelStudio@users.noreply.github.com>
2024-08-13 09:34:32 -0400

    Update windows-install.ps1

--- windows-install.ps1
+++ windows-install.ps1
@@ -48,7 +48,7 @@ foreach ($file in $fileDetails) {
     $filePath = $file.Path
     $url = "https://drive.google.com/uc?export=download&id=$fileId"
     
-    Write-Output "Downloading file"
+    Write-Output "Downloading file: $url"
     Write-Output "Saving to: $filePath"
     
     # Perform download
